7af893825d60e94887b8c20573ef3be135754482
joshpetit <joshuapetitma@yahoo.com>
2020-07-24 09:33:01 -0400

    Wrote basic test

--- src/test/java/com/github/joshpetit/simplemicrophoneapi/RecordTest.java
+++ src/test/java/com/github/joshpetit/simplemicrophoneapi/RecordTest.java
@@ -1,7 +1,33 @@
 package com.github.joshpetit.simplemicrophoneapi;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RecordTest {
+    Record record;
+    Microphone microphone;
+
+    @BeforeAll
+    public void create() {
+        microphone = new SimpleMicrophone();
+    }
+
+    @BeforeEach
+    void init() {
+        record = microphone.createRecordingBlob();
+    }
+
+    @DisplayName("Test Record")
+    @Test
+     void testRecord() throws InterruptedException {
+        record.startRecord();
+        Thread.sleep(500);
+        record.stopRecord();
+        assertNotEquals(0, record.getAudioFile().getTotalSpace());
+    }
 
 }
